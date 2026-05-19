e1c8b5cdc4b921d1892ec4bdb020111ee077e708
TheVictor <ce-vfokyere4721@st.umat.edu.gh>
2026-05-19 12:02:21 +0000

    update icon SVG path for improved design consistency

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -163,7 +163,7 @@ export default function App() {
                 <div className="flex items-start gap-3">
                   <div className="w-8 h-8 bg-orange-500 rounded flex items-center justify-center flex-shrink-0 mt-1">
                     <svg className="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 24 24">
-                      <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
+                      <path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.5-13H11v6l5.25 3.15.75-1.23-4.5-2.67z"/>
                     </svg>
                   </div>
                   <div>
